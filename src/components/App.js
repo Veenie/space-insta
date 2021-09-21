5aa6e37f118c84e2cc40e0f0784eda9b2d21e214
Veenie <raveen08@gmail.com>
2021-09-20 19:00:47 -0700

    audit and added browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 
 import '../styles/App.scss';
+import Navigation from "./Navigation";
+import Cards from "./Cards";
+
 
 function App() {
   return (
     <div className="App">
+            <Navigation />
+      <main>
+        <div className="sub">
+          <Cards />
+        </div>
+      </main>
  
     </div>
   );
